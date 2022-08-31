f726a4a519cc23bce8d99c0711bd0f382394c75b
MarcosCInland <marcos.andres@inlandlogistics.co>
2022-08-30 23:49:32 -0600

    map box full screen

--- src/app/mapas/pages/full-screen/full-screen.component.ts
+++ src/app/mapas/pages/full-screen/full-screen.component.ts
@@ -1,16 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-full-screen',
   templateUrl: './full-screen.component.html',
-  styles: [
-  ]
+  styles: [`
+    #mapa {
+      height: 100%;
+      width: 100%;
+    }
+  `]
 })
 export class FullScreenComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
+
+    var map = new mapboxgl.Map({
+      container: 'mapa',
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [-90.51781935984253, 14.62318186534125], //longitud, latitud
+      zoom: 12
+    });
   }
 
 }
